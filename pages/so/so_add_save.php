9e5b1b52c7312e1a9f9634d57c77effea715ec4c
dhuka <dhuka@ssonlab.com>
2018-08-14 05:04:39 +0700

    update po

--- pages/so/so_add_save.php
+++ pages/so/so_add_save.php
@@ -11,8 +11,8 @@
         //$kodeawal = 'est_'.$hrini.'_';
         $hrn2= date('dmy' , strtotime($hrini));
   //EST.BR.020818.000001
-        $kodeawal2 = 'SO_BR.';
-        $kodeawal = 'SO_BR.'.$hrn2.'.';
+        $kodeawal2 = 'SO_MH.';
+        $kodeawal = 'SO_MH.'.$hrn2.'.';
         //$sqljur = "SELECT * FROM t_penjualan WHERE id_penjualan LIKE '$kodeawal%' ORDER BY id_penjualan DESC";
         $sqljur = "SELECT * FROM t_penjualan WHERE id_penjualan LIKE '$kodeawal2%' ORDER BY id_penjualan DESC";
         $resultjur = mysql_query( $sqljur );
