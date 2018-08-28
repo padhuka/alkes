d6f6506ebfdf42c6b814ec0276dc06791cb42f97
dhuka <dhuka@ssonlab.com>
2018-08-29 06:26:41 +0700

    check stok befor add

--- pages/so/barang_add_save.php
+++ pages/so/barang_add_save.php
@@ -9,8 +9,40 @@
         $qty= trim($_POST['qty']);
         $hargadiskon= ($diskon*$hargajual)/100;
         $total= trim($_POST['hargatotal']);
-      
-		    $sqltbemp = "INSERT INTO t_penjualan_detail (fk_penjualan,fk_barang,gross_jual_barang,diskon_jual_barang,netto_jual_barang,qty) VALUES ('$idso','$id_barang','$hargajual','$hargadiskon','$total','$qty')";
+            
+
+            $checkstock  ="SELECT (FX.qty - ifnull(X.qty,0)) as qty FROM (
+                                       SELECT * FROM
+                                       (
+                                       SELECT pd.fk_barang,b.nama, sum(pd.qty) as qty,cast('masuk' as char(5))as tipe from t_pembelian_detail pd
+                                       LEFT  join t_pembelian p ON p.id_pembelian=pd.fk_pembelian  
+                                       LEFT  join t_barang b ON pd.fk_barang=b.id_barang
+                                       WHERE p.tgl_batal='0000-00-00 00:00:00' AND pd.fk_barang='$id_barang' GROUP BY pd.fk_barang
+                                      UNION
+                                       SELECT od.fk_barang,g.nama,od.qty as qty,cast('keluar' as char(6))as tipe from t_delivery_order_detail od
+                                       LEFT join t_delivery_order o ON o.id_delivery_order=od.fk_delivery_order
+                                       LEFT join t_barang g ON od.fk_barang=g.id_barang
+                                       WHERE o.tgl_batal='0000-00-00 00:00:00' AND od.fk_barang='$id_barang' GROUP BY od.fk_barang) as STOK WHERE tipe='masuk') AS FX
+                                       LEFT JOIN  (SELECT * FROM (
+                                        SELECT * FROM
+                                       (
+                                       SELECT pd.fk_barang,b.nama,sum(pd.qty) as qty,cast('masuk' as char(5))as tipe from t_pembelian_detail pd
+                                       LEFT  join t_pembelian p ON p.id_pembelian=pd.fk_pembelian  
+                                       LEFT  join t_barang b ON pd.fk_barang=b.id_barang
+                                       WHERE p.tgl_batal='0000-00-00 00:00:00' AND pd.fk_barang='$id_barang' GROUP BY pd.fk_barang
+                                      UNION
+                                       SELECT od.fk_barang,g.nama,od.qty as qty,cast('keluar' as char(6))as tipe from t_delivery_order_detail od
+                                       LEFT join t_delivery_order o ON o.id_delivery_order=od.fk_delivery_order
+                                        LEFT join t_barang g ON od.fk_barang=g.id_barang
+                                       WHERE o.tgl_batal='0000-00-00 00:00:00' AND od.fk_barang='$id_barang' GROUP BY od.fk_barang) as STOK WHERE tipe='keluar') AS PX ) AS X         
+                                       ON FX.fk_barang=X.fk_barang"
+		   
+            $stock =  mysql_fetch_array(mysql_query($checkstock)); 
+            echo $stock; 
+            echo $qty;
+
+            if ($qty >= $stock) {
+            $sqltbemp = "INSERT INTO t_penjualan_detail (fk_penjualan,fk_barang,gross_jual_barang,diskon_jual_barang,netto_jual_barang,qty) VALUES ('$idso','$id_barang','$hargajual','$hargadiskon','$total','$qty')";
             mysql_query($sqltbemp);
             
             //echo 'n';
@@ -25,5 +57,12 @@
 
             $updatebarang = "UPDATE t_penjualan set total_gross_jual_barang='$totgrosbarang', total_diskon_jual_barang='$totdiskonbarang', total_netto_jual_barang='$totnettobarang' WHERE id_penjualan='$idso'";
             mysql_query($updatebarang);
+
+            }
+            else {
+                echo "Barang Tidak Mencukupi";
+            }
+
+           
 
 ?>
